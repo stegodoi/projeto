40dddf3221d283d302f10d5f633ab04dfc0cf007
Steffanie Godoi <stelgodoi@gmail.com>
2023-10-19 15:47:56 -0300

    change avatar photo

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function toggleMode (){
     const img = document.querySelector('#profile img')
     
     if(html.classList.contains('light')){
-       img.setAttribute('src', './avatar-light.png')
+       img.setAttribute('src', './avatar-ste-dark.png')
     } else {
         img.setAttribute('src', './avatarstef.png')
     }
